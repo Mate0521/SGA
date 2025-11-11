ad99bd7a22dec5babc5764557b3603b34af1f615
FIELDGUITAR2 <josagoor@gmail.com>
2025-11-10 20:39:09 -0500

    Arreglos a los modelos

--- Modelos/Alumno.php
+++ Modelos/Alumno.php
@@ -1,52 +1,12 @@
 <?php
-    class Alumno extends Persona {
-        private $legajo;
-        private $curso;
+class Alumno extends Persona
+{
 
-        public function __construct($nombre = "", $id = "", $correo = "", $clave = "", $foto = "", $legajo = "", $curso = ""){
-            parent::__construct($nombre, $id, $correo, $clave, $foto);
-            $this->legajo = $legajo;
-            $this->curso = $curso;
-        }
 
-        /**
-         * Get the value of legajo
-         */ 
-        public function getLegajo()
+        public function __construct($nombre = "", $id = "", $correo = "", $clave = "", $foto = "", $legajo = "", $curso = "")
         {
-                return $this->legajo;
+                parent::__construct($nombre, $id, $correo, $clave, $foto);
+
         }
-
-        /**
-         * Set the value of legajo
-         *
-         * @return  self
-         */ 
-        public function setLegajo($legajo)
-        {
-                $this->legajo = $legajo;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of curso
-         */ 
-        public function getCurso()
-        {
-                return $this->curso;
-        }
-
-        /**
-         * Set the value of curso
-         *
-         * @return  self
-         */ 
-        public function setCurso($curso)
-        {
-                $this->curso = $curso;
-
-                return $this;
-        }
-    }
+}
 ?>
