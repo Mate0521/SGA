615d06e039b599b1ce3d18633d76131a3ec2a1ac
FIELDGUITAR2 <josagoor@gmail.com>
2025-11-12 21:40:49 -0500

    ultimos cambios

--- API/src/ws/Modelos/Alumno.php
+++ API/src/ws/Modelos/Alumno.php
@@ -1,8 +1,6 @@
 <?php
 class Alumno extends Persona
 {
-
-
         public function __construct($nombre = "", $id = "", $correo = "", $clave = "", $foto = "", $legajo = "", $curso = "")
         {
                 parent::__construct($nombre, $id, $correo, $clave, $foto);
