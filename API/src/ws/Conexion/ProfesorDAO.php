ed4d426f6791610163d0eb0b1514a23e31e31518
FIELDGUITAR2 <josagoor@gmail.com>
2025-11-17 11:01:30 -0500

    ProfesoresLista

--- API/src/ws/Conexion/ProfesorDAO.php
+++ API/src/ws/Conexion/ProfesorDAO.php
@@ -1,8 +1,52 @@
 <?php
-    class ProfesorDAO extends PersonaDAO {
+class ProfesorDAO extends PersonaDAO
+{
 
-        public function __construct($nombre = "", $id = "", $correo = "", $clave = "", $foto = "", $legajo = "", $materia = ""){
-            parent::__construct($nombre, $id, $correo, $clave, $foto);
-        }
+    public function __construct($nombre = "", $id = "", $correo = "", $clave = "", $foto = "", $legajo = "", $materia = "")
+    {
+        parent::__construct($nombre, $id, $correo, $clave, $foto);
     }
-?>
+
+    /*
+        1. una clase exclusiva para conslta del listado de profesores (no le voy ha enviar nada ).
+
+        datos que me debe de dar (con el mismo nombre ):
+        nombre: nombre del profesor 
+        correo: correo del prof
+        telefono: telefono del pof
+        area: nombre del area del profesor
+        departamento: nombre del departamento del area 
+        cursos:{}->un listado con el nombre de los cursos que da sigue estando en formato json
+        */
+
+    public function ListaProfesores()
+    {
+        return "SELECT 
+                p.Nombre_Profesor, 
+                p.Correo, 
+                a.Nombre_Area, 
+                d.Nombre_Departamento
+                FROM Profesor p
+                INNER JOIN Area_Con a 
+                    ON p.Id_AreaCon = a.Id_Area
+                INNER JOIN Departamento d 
+                    ON a.Id_Departamento = d.Id_Departamento
+                WHERE p.Id_Profesor = ".$this->getId().";
+                ";
+    }
+
+    public function ListaMaterias()
+    {
+        return "
+        SELECT
+            a.Nombre_Asignatura,
+            p.Nombre_Profesor
+            FROM
+            Profesor p 
+            INNER JOIN 
+            Asignatura_Profesor ap on p.Id_Profesor = ap.Id_Profesor
+            INNER JOIN
+            Asignatura a on ap.Id_Asignatura = a.Id_Asignatura
+            WHERE p.Id_Profesor = ".$this->getId();
+    }
+}
--- API/src/ws/Modelos/Profesor.php
+++ API/src/ws/Modelos/Profesor.php
@@ -1,8 +1,95 @@
 <?php
+    require_once '../Conexion/Conexion.php';
+    require_once '../Conexion/ProfesorDAO.php';
+    require_once '../Modelos/Persona.php';
+    require_once '../Modelos/AreaCon.php';  
     class Profesor extends Persona {
+        private $idArea;
+        private $Tel;
+        private $profesores = array();
 
-        public function __construct($nombre = "", $id = "", $correo = "", $clave = "", $foto = "", $legajo = "", $materia = ""){
+        public function __construct($nombre = "", $id = "", $correo = "", $clave = "", $foto = "", $idArea = "", $Tel = ""  ){
             parent::__construct($nombre, $id, $correo, $clave, $foto);
+            $this->idArea = $idArea;
+            $this->Tel = $Tel;
+        }
+
+        public function listadoProfesores(){
+            $conexion = new Conexion();
+            $profesorDAO = new ProfesorDAO();
+            
+            $conexion->abrir();
+            $conexion->ejecutar($profesorDAO->ListaProfesores());
+            
+            while(($datos = $conexion->registro())!=null){
+                $areaCon = new AreaCon("",$datos[3],$datos[2]);
+                $profe = new Profesor(
+                    $datos[0],
+                    $this->getId(),
+                    $datos[1],
+                    "",
+                    "",
+                    $areaCon,
+                    ""                    
+                );
+                array_push($profesores,$profe);
+            }
+
+            while(($datos = $conexion->registro())!=null){
+                $areaCon = new AreaCon("",$datos[3],$datos[2]);
+                $profe = new Profesor(
+                    $datos[0],
+                    $this->getId(),
+                    $datos[1],
+                    "",
+                    "",
+                    $areaCon,
+                    ""                    
+                );
+                array_push($profesores,$profe);
+            }
+
+            $conexion->cerrar();
+        }
+
+        /**
+         * Get the value of idArea
+         */ 
+        public function getIdArea()
+        {
+                return $this->idArea;
+        }
+
+        /**
+         * Set the value of idArea
+         *
+         * @return  self
+         */ 
+        public function setIdArea($idArea)
+        {
+                $this->idArea = $idArea;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of Tel
+         */ 
+        public function getTel()
+        {
+                return $this->Tel;
+        }
+
+        /**
+         * Set the value of Tel
+         *
+         * @return  self
+         */ 
+        public function setTel($Tel)
+        {
+                $this->Tel = $Tel;
+
+                return $this;
         }
     }
 ?>
