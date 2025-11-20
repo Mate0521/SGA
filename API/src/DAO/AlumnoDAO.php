2fa68a1349c05e46a9d7c37cba5c1e7f6caea151
FIELDGUITAR2 <josagoor@gmail.com>
2025-11-20 12:35:23 -0500

    acomodacion

--- API/src/DAO/AlumnoDAO.php
+++ API/src/DAO/AlumnoDAO.php
@@ -10,25 +10,25 @@ class AlumnoDAO extends PersonaDAO
         public function obtenerCursosYHorarios($correo)
         {
                 return "SELECT 
-    a.Nombre_Alumno AS nombre_alumno,
-    a.Correo AS correo,
-    c.id_curso,
-    asi.Nombre_Asignatura AS asignatura,
-    p.Nombre_Profesor AS profesor,
-    c.lunes,
-    c.martes,
-    c.miercoles,
-    c.jueves,
-    c.viernes,
-    c.sabado,
-    c.domingo
-FROM alumno a
-INNER JOIN alumno_horario ah ON a.Id_Alumno = ah.Id_Alumno
-INNER JOIN curso c ON ah.id_curso = c.id_curso
-INNER JOIN curso_profesor cp ON c.id_curso = cp.id_curso
-INNER JOIN profesor p ON cp.Id_Profesor = p.Id_Profesor
-INNER JOIN asignatura asi ON c.id_asignatura = asi.Id_Asignatura
-WHERE a.Correo = '$correo';
+                            a.Nombre_Alumno AS nombre_alumno,
+                            a.Correo AS correo,
+                            c.id_curso,
+                            asi.Nombre_Asignatura AS asignatura,
+                            p.Nombre_Profesor AS profesor,
+                            c.lunes,
+                            c.martes,
+                            c.miercoles,
+                            c.jueves,
+                            c.viernes,
+                            c.sabado,
+                            c.domingo
+                        FROM alumno a
+                        INNER JOIN alumno_horario ah ON a.Id_Alumno = ah.Id_Alumno
+                        INNER JOIN curso c ON ah.id_curso = c.id_curso
+                        INNER JOIN curso_profesor cp ON c.id_curso = cp.id_curso
+                        INNER JOIN profesor p ON cp.Id_Profesor = p.Id_Profesor
+                        INNER JOIN asignatura asi ON c.id_asignatura = asi.Id_Asignatura
+                        WHERE a.Correo = '$correo';
 ";
         }
 }
